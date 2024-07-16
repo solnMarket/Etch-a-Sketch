41758039047aaa2084c157548054d9058cc0bb2e
jezus <90801079+solnMarket@users.noreply.github.com>
2024-07-16 13:29:33 +0800

    created the function that makes sets the grid
    it was tough it took me weeks but i realize i was just procrastinating
    I didnt want to do any work i finished it in an hour lol

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
 
+
+
+
+
+
+
+
+
+
+function setGrid(){
+
+
+    const size = document.querySelector(".section2");
+    let h = size.clientHeight;
+    let w = size.clientWidth;
+
+
+    grid = prompt("enter desired grid size?")
+    count_items = grid * grid;
+    let tmp = h/grid
+    let tmp1 =w/grid
+
+    for(let i=0; i<count_items;i++){
+        const item = document.createElement('div');
+        item.style.height=tmp+'px'
+        item.style.width=tmp+'px'
+        item.classList.add('effect')
+        size.appendChild(item)
+
+    }
+     
+}
+
+const b = document.querySelector(".grid-change") 
+b.addEventListener("click",setGrid);
