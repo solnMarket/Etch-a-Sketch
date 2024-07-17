d73ed8cd693cc31bd95505dfd56eef9506a0e1a9
jezus <90801079+solnMarket@users.noreply.github.com>
2024-07-17 21:38:16 +0800

    complete project.
    a button that creates a grid, when clicked again it deletes the existing grid and creates a new
    when the mouse hovers on a div it changes its color from white to vblack.
    the clear button removes the existing color on the div to white.

--- main.js
+++ main.js
@@ -9,11 +9,18 @@
 
 
 function setGrid(){
+    let arr = document.querySelectorAll(".effect")
+    let arr1 = Array.from(arr)
 
 
     const size = document.querySelector(".section2");
     let h = size.clientHeight;
     let w = size.clientWidth;
+
+    for(let z=0;z<arr1.length;z++){
+        size.removeChild(arr1[z])
+    }
+    
 
 
     grid = prompt("enter desired grid size?")
@@ -26,11 +33,25 @@ function setGrid(){
         item.style.height=tmp+'px'
         item.style.width=tmp+'px'
         item.classList.add('effect')
+        item.addEventListener("mouseenter", function() {
+            this.style.backgroundColor="black"
+        })
         size.appendChild(item)
 
     }
      
 }
 
+function c(){
+    let arr = document.querySelectorAll(".effect")
+    let arr1 = Array.from(arr)
+    for(let h=0;h<arr1.length;h++){
+        arr1[h].style.backgroundColor="white"
+}
+}
+
 const b = document.querySelector(".grid-change") 
 b.addEventListener("click",setGrid);
+
+const clear = document.querySelector(".clear")
+clear.addEventListener("click",c)
